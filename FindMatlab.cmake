8601ad04fe08c45751d8f79d69b9f9874a777bbf
Nicholas Palko <npalko@tepper.cmu.edu>
2010-05-24 01:30:47 -0500

    a start in a proper search for matlab libs

--- FindMatlab.cmake
+++ FindMatlab.cmake
@@ -1,2 +1,23 @@
-# replicate functionality found in mex compilier, so you don't need matlab
-# to build matlab modules
+# Find MATLAB
+#
+# Usage: find_package (Matlab)
+#
+#  MATLAB_FOUND           TRUE if found
+#  MATLAB_INCLUDE_DIR     mex.h
+#  MATLAB_LIBRARY         libmx, libmex, libmat
+
+
+find_path(MATLAB_INCLUDE_DIR mex.h
+  $ENV{PROGRAMFILES}/MATLAB/R2009a/extern/include
+  )
+
+find_path(MATLAB_LIBRARY libmx${CMAKE_STATIC_LIBRARY_SUFFIX} 
+  $ENV{PROGRAMFILES}/MATLAB/R2009a/extern/lib/win32/microsoft
+  )
+
+set (MATLAB_FOUND TRUE)
+#set (MATLAB_INCLUDE_DIR ${MATLAB_INCLUDE_DIR})
+#set (MATLAB_LIBRARY ${MATLAB_LIBRARY})
+
+
+
