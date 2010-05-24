8f4d1229c5dbf6bc42167bdd075d1b0a8718d515
Nicholas Palko <npalko@tepper.cmu.edu>
2010-05-24 01:31:35 -0500

    placeholder until i figure out a better way to do this

---
+++ FindZeroMQ.cmake
@@ -0,0 +1,5 @@
+# Try to find ZeroMQ (libzmq)
+
+set (ZMQ_FOUND TRUE)
+set (ZMQ_INCLUDE_DIR "C:/Users/Nicholas Palko/Documents/Development/zeromq2/include")
+set (ZMQ_LIBRARY "C:/Users/Nicholas Palko/Documents/Development/zeromq2/lib")
